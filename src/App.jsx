32e0bd6ee54c0cec8e5a6d3b5021c29805be257d
Farhan <mansurifarhanfm@gmail.com>
2023-02-03 12:45:06 +0530

    Fixed the import BUG

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import HeroSection, { ImageSvg } from './components/HeroSection'
 import Navbar from './components/Navbar'
 import BenefitsSection from './components/BenefitsSection'
 import WhyComponent from './components/WhyComponent'
@@ -9,7 +7,6 @@ import CircleSVG from './components/CircleSVG'
 import Hero from './components/Hero'
 import Signin from './components/Signin'
 import styled from 'styled-components'
-import Illustrations from './components/BenefitsSection/Illustrations'
 
 const Container = styled.div`
 overflow: hidden;
